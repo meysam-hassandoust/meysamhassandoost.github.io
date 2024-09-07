7886170fb4695832bf392f1d2e7459b7dc7b7e9e
meysam-hassandoust <111142607+meysam-hassandoust@users.noreply.github.com>
2024-09-07 19:29:09 +0330

    Changes

---
+++ src/qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"66dc77512fc7070018bb958c",
+				"66dc77512fc7070018bb958f"
+			]
+		},
+		"66dc77512fc7070018bb958c": {
+			"id": "66dc77512fc7070018bb958c",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"66dc77512fc7070018bb958f": {
+			"id": "66dc77512fc7070018bb958f",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
